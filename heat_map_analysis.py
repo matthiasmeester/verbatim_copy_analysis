dc38968e9a947b043395c2fc0ea12cd0d28678f2
mmeester <matthiasmeester@gmail.com>
2022-05-14 13:19:29 +0200

    Change contouring to connectedComponents in patchStats

--- heat_map_analysis.py
+++ heat_map_analysis.py
@@ -16,29 +16,29 @@ class HeatMapAnalysis:
     def above_treshold_heat_index(self, threshold=0.7):
         return np.sum(self.heat_map >= threshold) / self.heat_map.size
 
-    def patch_stats(self, heat_treshold=0.5, patch_size_treshold=1, plot=False):
+    def patch_stats(self, heat_treshold=0.75, patch_size_treshold=1, plot=False):
         smoothed_heat_map = VerbatimHeatMapCreator.smooth_heat_map(self.heat_map, 2, 1)
         filtered_heat_map = np.where(smoothed_heat_map > heat_treshold, 1, 0)
         pixel_map = (np.array(filtered_heat_map * 255)).astype(np.uint8)
         rgb_pixel_map = cv2.cvtColor(pixel_map, cv2.COLOR_GRAY2RGB)
 
-        contours, hierarchy = cv2.findContours(pixel_map, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        patch_sizes = []
+        ret, thresh = cv2.threshold(pixel_map, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh)
 
-        # Loop over the found bounding boxes of patches
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            # Count the number of activated pixels in the bounding box; this is the patch size
-            patch_size = np.sum(filtered_heat_map[y:y + h, x:x + w])
-            if patch_size > patch_size_treshold:
-                patch_sizes.append(patch_size)
+        largest_patch_size = 0
+        for i in range(1, n_labels):
+            patch_area = stats[i, cv2.CC_STAT_AREA]
+            if patch_area >= patch_size_treshold:
+                largest_patch_size = max(patch_area, largest_patch_size)
                 if plot:
+                    x = stats[i, cv2.CC_STAT_LEFT]
+                    y = stats[i, cv2.CC_STAT_TOP]
+                    w = stats[i, cv2.CC_STAT_WIDTH]
+                    h = stats[i, cv2.CC_STAT_HEIGHT]
                     cv2.rectangle(rgb_pixel_map, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
-
         if plot:
             plt.figure()
+            plt.title('Verbatim patches')
+            plt.axis('off')
             plt.imshow(rgb_pixel_map)
-
-        number_of_boxes, largest_box_size = len(patch_sizes), max(patch_sizes)
-
-        return number_of_boxes, largest_box_size
+        return n_labels, largest_patch_size
