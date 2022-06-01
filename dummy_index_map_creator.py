8ca4d1704eb90b50ded3d542795b6add1f745bd7
mmeester <matthiasmeester@gmail.com>
2022-06-01 12:33:46 +0200

    Add a seperate file for creating dummy maps

---
+++ dummy_index_map_creator.py
@@ -0,0 +1,98 @@
+import random
+from random import randint
+from random import randrange
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DummyIndexMapCreator:
+
+    def __init__(self, map_shape):
+        self.map_shape = map_shape
+        self.source_index = np.stack(
+            np.meshgrid(np.arange(self.map_shape[0]) / self.map_shape[0],
+                        np.arange(self.map_shape[1]) / self.map_shape[1]) + [
+                np.ones(self.map_shape)], axis=-1)
+
+    def create_checkerboard_map(self, square_size, plot=False):
+        full_random = self.create_full_random_map()
+        full_verbatim = self.create_full_verbatim_map()
+        n_rows, n_columns = full_random.shape[0], full_random.shape[1]
+        n_rows_, n_columns_ = int(n_rows / square_size + 1), int(n_columns / square_size + 1)
+        rows_grid, columns_grid = np.meshgrid(range(n_rows_), range(n_columns_), indexing='ij')
+        high_res_checkerboard = np.mod(rows_grid, 2) + np.mod(columns_grid, 2) == 1
+        square = np.ones((square_size, square_size))
+        checkerboard = np.kron(high_res_checkerboard, square)[:n_rows, :n_columns]
+        checkerboard = np.where(checkerboard, full_verbatim, full_random).astype(np.int32)
+
+        if plot:
+            plt.imshow(np.reshape(self.source_index, (-1, 3))[checkerboard])
+            plt.title('10x10 Checkerboard dummy index map')
+            plt.axis('off')
+            plt.savefig('output/checkerboard_index_map', dpi=150)
+            plt.show()
+
+        return checkerboard
+
+    def create_patch_map(self, num_patches, noise=0.05, plot=False):
+        full_random = self.create_full_random_map()
+        full_verbatim = self.create_full_verbatim_map()
+        result = full_random.copy()
+        is_verbatim = np.full(result.shape, False)
+        total_size = is_verbatim.shape[0] * is_verbatim.shape[1]
+        lr, rr = 5, 25
+        for _ in range(num_patches):
+            h, w = full_random.shape[:2]
+            circle_pos_x, circle_pos_y = randrange(w), randrange(h)
+            circle_pos_copy_x, circle_pos_copy_y = randrange(2 * rr, w - 2 * rr), randrange(2 * rr, h - 2 * rr)
+            shift_x = circle_pos_copy_x - circle_pos_x
+            shift_y = circle_pos_copy_y - circle_pos_y
+            circle_r = randint(5, 25)
+
+            for iy, ix in np.ndindex(result.shape):
+                # Check if point is on circle and put some noise
+                if (ix - circle_pos_x) ** 2 + (iy - circle_pos_y) ** 2 <= circle_r ** 2 and random.random() >= noise:
+                    result[iy, ix] = full_verbatim[iy + shift_y, ix + shift_x]
+                    is_verbatim[iy, ix] = True
+
+        verbatim_copy_proportion = is_verbatim.sum() / total_size
+
+        if plot:
+            plt.imshow(np.reshape(self.source_index, (-1, 3))[result])
+            plt.title(f'Patch dummy index map, proportion verbatim = {verbatim_copy_proportion}')
+            plt.axis('off')
+            plt.savefig('output/patch_index_map', dpi=150)
+            plt.show()
+
+        return result, verbatim_copy_proportion
+
+    def create_long_range_map(self, proportion, plot=False):
+        result = self.create_full_random_map()
+        full_verbatim = self.create_full_verbatim_map()
+        is_verbatim = np.full(result.shape, False)
+        total_size = is_verbatim.shape[0] * is_verbatim.shape[1]
+
+        for iy, ix in np.ndindex(result.shape):
+            if random.random() < proportion:
+                is_verbatim[iy, ix] = True
+                result[iy, ix] = full_verbatim[iy, ix]
+
+        verbatim_copy_proportion = is_verbatim.sum() / total_size
+
+        if plot:
+            plt.imshow(np.reshape(self.source_index, (-1, 3))[result])
+            plt.title(f'Long range index map, proportion verbatim = {round(verbatim_copy_proportion, 3)}')
+            plt.axis('off')
+            plt.savefig('output/long_range_index_map', dpi=150)
+            plt.show()
+
+        return result, verbatim_copy_proportion
+
+    def create_full_verbatim_map(self):
+        total_size = self.map_shape[0] * self.map_shape[1]
+        return np.arange(0, total_size).reshape(self.map_shape)
+
+    def create_full_random_map(self):
+        total_size = self.map_shape[0] * self.map_shape[1]
+        return (np.random.rand(self.map_shape[0], self.map_shape[1]) * total_size).astype(np.int32)
--- verbatim_copy.py
+++ verbatim_copy.py
@@ -1,11 +1,10 @@
 import os.path
-import random
-from random import randint
 from random import randrange
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+from dummy_index_map_creator import DummyIndexMapCreator
 from heat_map_analysis import HeatMapAnalysis
 from verbatim_heat_map_creator import VerbatimHeatMapCreator
 
@@ -13,58 +12,12 @@ from verbatim_heat_map_creator import VerbatimHeatMapCreator
 # --- Custom variables ---
 inv_dist_weight_exp = 2
 
-
 # seed(123456)
 # ---
 
 # From: https://stackoverflow.com/questions/38083788/turn-grid-into-a-checkerboard-pattern-in-python
-def make_checkerboard(full_copy, full_random, square_size):
-    n_rows, n_columns = full_random.shape[0], full_random.shape[1]
-    n_rows_, n_columns_ = int(n_rows / square_size + 1), int(n_columns / square_size + 1)
-    rows_grid, columns_grid = np.meshgrid(range(n_rows_), range(n_columns_), indexing='ij')
-    high_res_checkerboard = np.mod(rows_grid, 2) + np.mod(columns_grid, 2) == 1
-    square = np.ones((square_size, square_size))
-    checkerboard = np.kron(high_res_checkerboard, square)[:n_rows, :n_columns]
-    checkerboard = np.where(checkerboard, full_copy, full_random)
-    return checkerboard.astype(np.int32)
 
-
-def put_patches(full_copy, full_random, num_patches, patch_radius, noise=0.05):
-    result = full_random.copy()
-    is_verbatim = np.full(result.shape, False)
-    total_size = is_verbatim.shape[0] * is_verbatim.shape[1]
-    lr, rr = 5, 25
-    for _ in range(num_patches):
-        h, w = full_random.shape[:2]
-        circle_pos_x, circle_pos_y = randrange(w), randrange(h)
-        circle_pos_copy_x, circle_pos_copy_y = randrange(2 * rr, w - 2 * rr), randrange(2 * rr, h - 2 * rr)
-        shift_x = circle_pos_copy_x - circle_pos_x
-        shift_y = circle_pos_copy_y - circle_pos_y
-        circle_r = randint(5, 25)
-
-        for iy, ix in np.ndindex(result.shape):
-            # Check if point is on circle and put some noise
-            if (ix - circle_pos_x) ** 2 + (iy - circle_pos_y) ** 2 <= circle_r ** 2 and random.random() >= noise:
-                result[iy, ix] = full_copy[iy + shift_y, ix + shift_x]
-                is_verbatim[iy, ix] = True
-
-    verbatim_copy_proportion = is_verbatim.sum() / total_size
-    return result, verbatim_copy_proportion
-
-
-def randomly_draw(base, to_sample_from, proportion):
-    result = base.copy()
-    is_verbatim = np.full(result.shape, False)
-    total_size = is_verbatim.shape[0] * is_verbatim.shape[1]
-
-    for iy, ix in np.ndindex(result.shape):
-        if random.random() < proportion:
-            is_verbatim[iy, ix] = True
-            result[iy, ix] = to_sample_from[iy, ix]
-
-    verbatim_copy_proportion = is_verbatim.sum() / total_size
-    return result, verbatim_copy_proportion
-
+index_map_creator = DummyIndexMapCreator((200, 200))
 
 directory = "simulations"
 for path in os.listdir(directory):
@@ -86,88 +39,7 @@ for path in os.listdir(directory):
             np.meshgrid(np.arange(ti.shape[0]) / ti.shape[0], np.arange(ti.shape[1]) / ti.shape[1]) + [
                 np.ones_like(ti)], axis=-1)
 
-        # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(6, 6))
-        # fig.suptitle(f'QS stone, k={k}', size='xx-large')
-        # ax1.imshow(ti)
-        # ax1.set_title('Training image')
-        # ax1.axis('off')
-        # ax2.imshow(simulation)
-        # ax2.set_title('Simulation')
-        # ax2.axis('off')
-        #
-        # ax3.imshow(sourceIndex)
-        # ax3.set_title('Training image index map')
-        # ax3.axis('off')
-        # ax4.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        # ax4.set_title('Simulation index map')
-        # ax4.axis('off')
-        # plt.savefig('output/input_example_index_map', dpi=150)
-        # plt.savefig('output/input_example', dpi=150)
-        # plt.show()
-        #
-        # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7, 4))
-        # ax1.imshow(sourceIndex)
-        # ax1.set_title('Training image')
-        # ax1.axis('off')
-        # ax2.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        # ax2.set_title('Simulation')
-        # ax2.axis('off')
-        # plt.savefig('output/input_example_index_map', dpi=150)
-        # plt.show()
-
-        # verbatim copy:
-        # simulation = ti.copy()
-        # index_map = np.arange(0, 40000).reshape((200, 200))
-
-        # Patches
-        index_map, verbatim_copy_proportion = put_patches(np.arange(0, 40000).reshape((200, 200)),
-                                                          (np.random.rand(200, 200) * 40000).astype(np.int32), 10, 30)
-        plt.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        plt.title(f'Patch dummy index map, proportion verbatim = {verbatim_copy_proportion}')
-        plt.axis('off')
-        plt.savefig('output/patch_index_map', dpi=150)
-        plt.show()
-
-        # Long range
-        # chance = 0.8
-        # index_map, verbatim_copy_proportion = randomly_draw((np.random.rand(200, 200) * 40000).astype(np.int32),
-        #                                                     np.arange(0, 40000).reshape((200, 200)), chance)
-        # plt.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        # plt.title(f'Long range index map, proportion verbatim = {round(verbatim_copy_proportion, 3)}')
-        # plt.axis('off')
-        # plt.savefig('output/long_range_index_map', dpi=150)
-        # plt.show()
-
-        # Randomness
-        # index_map = (np.random.rand(200, 200) * 40000).astype(np.int32)
-        # plt.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        # plt.title('Random dummy index map')
-        # plt.axis('off')
-        # plt.savefig('output/random_index_map', dpi=150)
-        # plt.show()
-
-        # Checkerboard
-        # index_map = make_checkerboard(np.arange(0, 40000).reshape((200, 200)),
-        #                               (np.random.rand(200, 200) * 40000), 10)
-        #
-        # plt.imshow(np.reshape(sourceIndex, (-1, 3))[index_map])
-        # plt.title('10x10 Checkerboard dummy index map')
-        # plt.axis('off')
-        # plt.savefig('output/checkerboard_index_map', dpi=150)
-        # plt.show()
-
-        # shuffled randomness:
-        # random_seed = np.random.randint(0, 1000)
-        # np.random.seed(random_seed)
-        # index_map = np.arange(0, 40000)
-        # np.random.shuffle(index_map)
-        # index_map = index_map.reshape((200, 200))
-        # np.random.seed(random_seed)
-        # simulation = simulation.reshape((40000, -1))
-        # np.random.shuffle(simulation)
-        # simulation = simulation.reshape((200, 200))
-
-        # TODO: Test in between for example 50% verbatim copy with different patterns, chessboard for example
+        index_map = index_map_creator.create_checkerboard_map(3)
 
         heat_map_creator = VerbatimHeatMapCreator(index_map, simulation)
         n_fr = 70
