fe90dfeeb5b54e811d9ecf601d009aed4402e22d
mmeester <matthiasmeester@gmail.com>
2022-06-01 13:52:22 +0200

    Reformat results generator

--- results_generator.py
+++ results_generator.py
@@ -10,6 +10,7 @@ index_map_creator = DummyIndexMapCreator((200, 200))
 max_range = int(sqrt(200 ** 2 + 200 * 2))
 result_df = pd.read_csv('output/results.csv')
 
+# Do 10 replications
 for run in range(10):
     patches_05, copy_proportion_05 = index_map_creator.create_patch_map(num_patches=5)
     patches_10, copy_proportion_10 = index_map_creator.create_patch_map(num_patches=10)
@@ -18,6 +19,7 @@ for run in range(10):
     long_range_50, proportion_50 = index_map_creator.create_long_range_map(0.50)
     long_range_75, proportion_75 = index_map_creator.create_long_range_map(0.75)
 
+    # Create different test cases
     test_cases = [
         {
             'map': index_map_creator.create_full_random_map(),
@@ -87,6 +89,7 @@ for run in range(10):
         }
     ]
 
+    # Run different algorithms on the test cases
     for test_case in test_cases:
         test_map = test_case['map']
         heat_map_creator = VerbatimHeatMapCreator(test_map)
@@ -100,33 +103,37 @@ for run in range(10):
             'run': run
         }
 
-        # Mean heat
+        # Mean heat algorithms
         mean_heat_d1_r1 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=1, inv_dist_weight_exp=1)).mean_heat_value()
         mean_heat_d2_r1 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=1, inv_dist_weight_exp=2)).mean_heat_value()
         mean_heat_d1_r2 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=2, inv_dist_weight_exp=1)).mean_heat_value()
         mean_heat_d2_r2 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=2, inv_dist_weight_exp=2)).mean_heat_value()
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "mean_heat_d=1|r=1",
-            'predicted_verbatim': mean_heat_d1_r1,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "mean_heat_d=1|r=1",
+                'predicted_verbatim': mean_heat_d1_r1,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "mean_heat_d=2|r=2",
-            'predicted_verbatim': mean_heat_d2_r2,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "mean_heat_d=2|r=2",
+                'predicted_verbatim': mean_heat_d2_r2,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "mean_heat_d=1|r=2",
-            'predicted_verbatim': mean_heat_d1_r2,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "mean_heat_d=1|r=2",
+                'predicted_verbatim': mean_heat_d1_r2,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "mean_heat_d=2|r=1",
-            'predicted_verbatim': mean_heat_d2_r1,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "mean_heat_d=2|r=1",
+                'predicted_verbatim': mean_heat_d2_r1,
+            }), ignore_index=True
         )
 
-        # Proportional neighbour analysis
+        # Proportional neighbour analysis algorithms
         prop_r_1_p_0_05 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=1, inv_dist_weight_exp=0)).above_treshold_heat_index(0.05)
         prop_r_2_p_0_05 = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=2, inv_dist_weight_exp=0)).above_treshold_heat_index(0.05)
         max_range_heat_map = HeatMapAnalysis(heat_map_creator.get_verbatim_heat_map_filter_basis(filter_radius=max_range, inv_dist_weight_exp=0))
@@ -134,30 +141,35 @@ for run in range(10):
         prop_r_max_p_0_01 = max_range_heat_map.above_treshold_heat_index(0.01)
         prop_r_max_p_0_001 = max_range_heat_map.above_treshold_heat_index(0.001)
 
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "prop_r=1|T=0.1",
-            'predicted_verbatim': prop_r_1_p_0_05,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "prop_r=1|T=0.1",
+                'predicted_verbatim': prop_r_1_p_0_05,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "prop_r=2|T=0.1",
-            'predicted_verbatim': prop_r_2_p_0_05,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "prop_r=2|T=0.1",
+                'predicted_verbatim': prop_r_2_p_0_05,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "prop_r=max|T=0.1",
-            'predicted_verbatim': prop_r_max_p_01,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "prop_r=max|T=0.1",
+                'predicted_verbatim': prop_r_max_p_01,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "prop_r=max|T=0.01",
-            'predicted_verbatim': prop_r_max_p_0_01,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "prop_r=max|T=0.01",
+                'predicted_verbatim': prop_r_max_p_0_01,
+            }), ignore_index=True
         )
-        result_df = result_df.append(dict(test_case_info, **{
-            'algorithm_name': "prop_r=max|T=0.001",
-            'predicted_verbatim': prop_r_max_p_0_001,
-        }), ignore_index=True
+        result_df = result_df.append(
+            dict(test_case_info, **{
+                'algorithm_name': "prop_r=max|T=0.001",
+                'predicted_verbatim': prop_r_max_p_0_001,
+            }), ignore_index=True
         )
 
         result_df.to_csv('output/results.csv', index=False)
